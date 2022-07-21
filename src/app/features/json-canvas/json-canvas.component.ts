7911d13611307bb9e0bf4f8dc11bc290ffdf226c
Marco Cabrera <highlandsmac@yahoo.com>
2022-07-20 21:57:21 -0500

    added values svg render to the canvas

--- src/app/features/json-canvas/json-canvas.component.ts
+++ src/app/features/json-canvas/json-canvas.component.ts
@@ -41,27 +41,26 @@ export class JsonCanvasComponent implements OnInit {
 
   public addSvgKeyToCanvas(value: string) {
     let text = this.canvas.text(value).id('key1')
-    text.font({ fill: '#000', family: 'Inconsolata' }).dy(50).x(50);
+    text.font({ fill: '#000', family: 'Inconsolata' }).y(50).x(50);
     let background = SVG('#key1');
     if (background) {
       const xaxis = background.node.clientWidth;
       const yaxis = background.node.clientHeight
-      const squareKey = this.canvas.rect(10, 10).fill('#faf0e6').dy(30).x(40).radius(10).stroke('#000');
+      const squareKey = this.canvas.rect(10, 10).fill('#faf0e6').y(30).x(40).radius(10).stroke('#000');
       squareKey.height(yaxis).width(xaxis + 20);
-
       text.front()
     }
   }
 
   public addSvgValueToCanvas(value: string) {
     let text = this.canvas.text(value).id('value1')
-    text.font({ fill: '#000', family: 'Inconsolata' }).dy(100).x(100);
+    text.font({ fill: '#000', family: 'Inconsolata' }).y(50).x(150);
     let background = SVG('#value1');
     if (background) {
       const xaxis = background.node.clientWidth;
       const yaxis = background.node.clientHeight
-      const squareValue = this.canvas.rect(10, 10).fill('#faf0e6').dy(30).x(40).radius(10).stroke('#000');
-      squareValue.height(yaxis).width(xaxis + 20);
+      const squareValue = this.canvas.rect(10, 10).fill('#faf0e6').y(30).x(140).radius(10).stroke('#000');
+      squareValue.height(yaxis).width(xaxis);
 
       text.front()
     }
