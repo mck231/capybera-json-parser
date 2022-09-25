a27039f48afbdee8afef696b98af9134787c5375
Marco Cabrera <highlandsmac@yahoo.com>
2022-09-24 23:13:32 -0500

    made home component more mobile freindly

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,3 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
@@ -10,40 +9,11 @@ import { Subject, takeUntil } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  destroyed = new Subject<void>();
-
-
-  constructor(private router: Router, breakpointObserver: BreakpointObserver) {
-    breakpointObserver
-      .observe([
-        Breakpoints.XSmall,
-        Breakpoints.Small,
-        Breakpoints.Medium,
-        Breakpoints.Large,
-        Breakpoints.XLarge,
-      ])
-      .pipe(takeUntil(this.destroyed))
-      .subscribe(result => {
-        for (const query of Object.keys(result.breakpoints)) {
-          if (result.breakpoints[query]) {
-            console.warn(result.breakpoints[query])
-            console.warn(query)
-            console.warn(Breakpoints.XSmall)
-            console.warn(result.breakpoints)
-            // if(result.breakpoints[query] == Breakpoints.XSmall ) {
-
-            // }
-          }
-        }
-      });
-   }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    
   }
 
-  ngOnDestroy() {
-    this.destroyed.next();
-    this.destroyed.complete();
+  ngOnInit(): void {
   }
 
 }
